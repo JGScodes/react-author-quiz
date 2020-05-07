37f5df7d14daaeb65441ebb6e1bc896e7257b64e
Jaime <jaimesof82@gmail.com>
2020-05-06 18:55:55 -0700

    Added the authors data and styles for the UI

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+// import { render } from '@testing-library/react';
+import AuthorQuiz from './';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('Author Quiz', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<AuthorQuiz />, div);
+  });
 });
+
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
---
+++ src/test-examples/Bankai.test.js
@@ -0,0 +1,6 @@
+// //Always set up a failing test first to make sure testing is working properly.
+// describe('When setting up testing', () => {
+//   it('should fail', () => {
+//     expect(1 + 1).toBe(3);
+//   });
+// });
